58372222d50c3c5c1a008e7493c25232006854c4
Benjamin Cooper <bencooper222@gmail.com>
2019-10-08 17:55:35 -0500

    add sidebar todo and css

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// TODO: make input fields, do basic things with them to *react*
+// implement https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=vanderbilt+&format=json&limit=1
 export default function Sidebar() {
-  return <div className="sidebar">Hello</div>;
+  return (
+    <div className="sidebar">
+      <h1>Locations</h1>
+    </div>
+  );
 }
