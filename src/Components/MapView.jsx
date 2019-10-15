83fd243c072cec8999c2d545e3ee3e4035f28245
Benjamin Cooper <bencooper222@gmail.com>
2019-10-15 18:57:23 -0500

    prepare for week3

--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import '../../node_modules/leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// this library's author is bad: https://github.com/PaulLeCam/react-leaflet/issues/453
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('../../node_modules/leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('../../node_modules/leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('../../node_modules/leaflet/dist/images/marker-shadow.png'),
+});
+
+// TODOs:
+
+// 1. Let's deploy what we got
+// 2. Figure out a way to get the latlng/name from Sidebar -> MapView
+// 3. Maybe render multiple marker/popup pairs?
 
 export default function MapView() {
-  return <div className="map"></div>;
+  const position = [36.1447, -86.8027];
+  return (
+    <Map className="map" center={position} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>Vanderbilt University</Popup>
+      </Marker>
+    </Map>
+  );
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,19 +3,30 @@ import { getResponse } from '../util';
 
 // TODO: make input fields, do basic things with them to *react*
 // implement https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=vanderbilt+&format=json&limit=1
+
 export default class Sidebar extends Component {
   constructor() {
     super();
-    this.state = { thing: 'hi' };
+    this.state = { thing: 729 };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  async componentDidMount() {}
+  async handleChange(e) {
+    console.log(e);
+    this.setState({
+      thing: (await getResponse(
+        `https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=${e.target.value}+&format=json&limit=1`,
+      ))[0].display_name,
+    });
+  }
 
   render() {
     return (
       <div className="sidebar">
         <h1>Locations</h1>
         <p>{this.state.thing}</p>
+        <input onChange={this.handleChange}></input>
       </div>
     );
   }
